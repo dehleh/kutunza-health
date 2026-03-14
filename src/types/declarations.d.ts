000faec56d2470d4ff3b24dfeef36e5460af69cf
dehleh <dehleh@users.noreply.github.com>
2026-03-15 00:00:10 +0100

    resolved event booking issue

--- src/types/declarations.d.ts
+++ src/types/declarations.d.ts
@@ -2,3 +2,4 @@ declare module '@react-native-async-storage/async-storage';
 declare module '@react-navigation/native';
 declare module '@react-navigation/native-stack';
 declare module '@react-navigation/bottom-tabs';
+declare module 'expo-file-system';
